8a9a59ff9123d8ae8f96950811a28eeb9c7879f0
Alina Kitaeva <akitaeva@gmail.com>
2024-12-08 19:35:46 -0500

    Adds dotenv package and tyoes. Extends vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react';
+import * as dotenv from 'dotenv';
+
+// Load environment variables from .env
+dotenv.config();
 
 export default defineConfig({
+  base: './',
   plugins: [react()],
+// Configuration for testing
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './vitest.setup.ts', // Optional test setup
+    setupFiles: './vitest.setup.ts', // Include setup file for testing utilities
   },
-});
+  // Base path for assets
+  publicDir: 'public', // Directory for static asset
+})
